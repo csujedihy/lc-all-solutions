b66a1717473bab8cd0e02c071c0f568a7a7bc181
Jedihy <csujedihy@users.noreply.github.com>
2017-06-14 23:49:01 -0500

    Update median-of-two-sorted-arrays.py

--- 004.median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 004.median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -5,17 +5,28 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        def helper(nums1, nums2, start, end, n):
-            pass
+        def helper(k, nums1, nums2, start1, start2):
+            if start1 >= len(nums1):
+                return nums2[start2 + k - 1]
+            if start2 >= len(nums2):
+                return nums1[start1 + k - 1]
+            if k == 1:
+                return min(nums1[start1], nums2[start2])
+            
+            m1 = start1 + k / 2 - 1
+            m2 = start2 + k / 2 - 1
+            mid1 = float("inf") if m1 >= len(nums1) else nums1[m1]
+            mid2 = float("inf") if m2 >= len(nums2) else nums2[m2]
+
+            if mid1 < mid2:
+                return helper(k - k / 2, nums1, nums2, m1 + 1, start2)
+            else:
+                return helper(k - k / 2, nums1, nums2, start1, m2 + 1)
+
         
-        m = len(nums1)
-        n = len(nums2)
-        half = (m + n) / 2
-        if m + n % 2 == 1:
-            return helper(nums1, nums2, start, end, half)
+        length = len(nums1) + len(nums2)
+        k = length / 2
+        if length % 2 == 0:
+            return (helper(k, nums1, nums2, 0, 0) + helper(k + 1, nums1, nums2, 0, 0)) / 2.0
         else:
-            ans = helper(nums1, nums2, start, end, half - 1)
-            ans += helper(nums1, nums2, start, end, half)
-            return ans / 2.0
-            
-            
+            return helper(k + 1, nums1, nums2, 0, 0)
