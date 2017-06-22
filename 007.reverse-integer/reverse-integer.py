299f569219d965f7137227a72600cbed77f630e3
Jedihy <csujedihy@users.noreply.github.com>
2017-06-22 18:29:24 -0500

    Update reverse-integer.py

--- 007.reverse-integer/reverse-integer.py
+++ 007.reverse-integer/reverse-integer.py
@@ -4,15 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        sign = 1
-        if x < 0:
-            sign = -1
-            x = -x
-            
-        res = 0
-        while x > 0:
-            res *= 10
-            res += x % 10
+        sign = x < 0 and -1 or 1
+        x = abs(x)
+        ans = 0
+        while x:
+            ans = ans * 10 + x % 10
             x /= 10
-            
-        return sign * res if abs(res) < 0x7fffffff else 0
+        return sign * ans if ans <= 0x7fffffff else 0
