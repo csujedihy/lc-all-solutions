60335db6a4e93ee462013ab3da1fa35f83e167ad
Jedihy <csujedihy@users.noreply.github.com>
2017-06-22 18:24:02 -0500

    Update zigzag-conversion.py

--- 006.zigzag-conversion/zigzag-conversion.py
+++ 006.zigzag-conversion/zigzag-conversion.py
@@ -5,21 +5,19 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-        if not s or numRows == 0:
-            return ""
-        if numRows == 1:
+        if numRows <= 1:
             return s
-        ans = ""
-        step = numRows * 2 - 2
-        for i in xrange(0, numRows):
-            if i == 0 or i == numRows - 1:
-                for j in xrange(i, len(s), step):
-                    ans += s[j]
-            else:
-                for j in xrange(i, len(s), step):
-                    ans += s[j]
-                    if j + step - 2 * i < len(s):
-                        ans += s[j + step - 2 * i]
-        return ans
-                    
-            
+        n = len(s)
+        ans = []
+        step = 2 * numRows - 2
+        for i in range(numRows):
+            one = i
+            two = -i
+            while one < n or two < n:
+                if 0 <= two < n and one != two and i != numRows - 1:
+                    ans.append(s[two])
+                if one < n:
+                    ans.append(s[one])
+                one += step
+                two += step
+        return "".join(ans)
