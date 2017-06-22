fb6f2d0dd0b28e65ab044e126d517ff13007b8b2
Jedihy <csujedihy@users.noreply.github.com>
2017-06-22 14:58:37 -0500

    updated optimized solutions

--- 621.task-scheduler/task-scheduler.py
+++ 621.task-scheduler/task-scheduler.py
@@ -1,43 +1,49 @@
-import heapq
-import collections
 class Solution(object):
-    def leastInterval(self, tasks, n):
+    
+    # O(nlogn) greedy to place most popular and distinct tasks first
+    # Actually, I don't think this is greedy
+    # We always place different tasks in a cycle which will minimize steps
+    # If not different tasks can be placed in a cycle, place an `idle`.
+    
+    def _leastInterval(self, tasks, n):
         """
         :type tasks: List[str]
         :type n: int
         :rtype: int
         """
-        total = len(tasks)
-        cache = {}
-        heap = []
+        n += 1
         ans = 0
         d = collections.Counter(tasks)
-        for k, v in d.iteritems():
-            heapq.heappush(heap, (-v, k))
-
+        heap = [-c for c in d.values()]
+        heapq.heapify(heap)
         while heap:
-            count, task = heap[0]
             stack = []
-            if task not in cache or ans - cache[task] > n:
-                heapq.heappop(heap)
-                total -= 1
-                cache[task] = ans
-                ans += 1
-                for _ in range(n):
-                    if heap:
-                        c, t = heapq.heappop(heap)
-                        if t not in cache or ans - cache[t] > n:
-                            total -= 1
-                            cache[t] = ans
-                        if c < -1:
-                            stack.append((c + 1, t))
-                    elif total == 0:
-                        return ans
-                    ans += 1
-                while stack:
-                    heapq.heappush(heap, stack.pop())
-                if count < -1:
-                    heapq.heappush(heap, (count + 1, task))
-            else:
-                ans += 1
+            cnt = 0
+            for _ in range(n):
+                if heap:
+                    c = heapq.heappop(heap)
+                    cnt += 1
+                    if c < -1:
+                        stack.append(c + 1)
+            for item in stack:
+                heapq.heappush(heap, item)
+            ans += heap and n or cnt # == if heap then n else cnt
         return ans
+        
+    
+    # O(n) # of the most frequent tasks, say longest, will determine the legnth
+    # to void counting idle intervals, we count (longest - 1) * (n + 1)
+    # then count how many will in the last cycle which means finding ties
+    # if counted number is less than # of tasks which means 
+    # less frequent tasks can be always placed in such cycle
+    # and it won't cause any conflicts with requirement since even most frequent can be settle
+    # finally, return max(# of task, total counted number)
+    
+    def leastInterval(self, tasks, n):
+        d = collections.Counter(tasks)
+        counts = d.values()
+        longest = max(counts)
+        ans = (longest - 1) * (n + 1)
+        for count in counts:
+            ans += count == longest and 1 or 0 # finding ties (one-line trick)
+        return max(len(tasks), ans)
