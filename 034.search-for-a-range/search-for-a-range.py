a7d4bedd0503ebf1a1a93a738e6528108549a0e1
Yi Huang <csujedi@icloud.com>
2022-04-10 16:22:39 -0700

    Update search-for-a-range.py

--- 034.search-for-a-range/search-for-a-range.py
+++ 034.search-for-a-range/search-for-a-range.py
@@ -1,42 +1,22 @@
-class Solution(object):
-  def searchRange(self, nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    l, r = 0, len(nums) - 1
-    found = 0
-    start, end = 0, 0
-    while l < r:
-      m = l + (r - l) / 2
-      if target > nums[m]:
-        l = m + 1
-      else:
-        if target == nums[m]:
-          found += 1
-        r = m - 1
-
-    if nums[l] == target:
-      found += 1
-
-    start = r
-    if nums[r] != target or r < 0:
-      start = r + 1
-
-    l, r = 0, len(nums) - 1
-    while l < r:
-      m = l + (r - l) / 2
-      if target < nums[m]:
-        r = m - 1
-      else:
-        if target == nums[m]:
-          found += 1
-        l = m + 1
-    end = l
-    if nums[l] != target:
-      end = l - 1
-
-    if found == 0:
-      return [-1, -1]
-    return [start, end]
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findRange(left):
+            l, r = 0, len(nums) - 1
+            m = 0
+            while l <= r:
+                m = l + (r - l) // 2
+                if nums[m] < target:
+                    l = m + 1
+                elif nums[m] > target:
+                    r = m - 1
+                elif left and m > 0 and nums[m - 1] == nums[m]:
+                    r = m - 1
+                elif not left and m + 1 < len(nums) and nums[m + 1] == nums[m]:
+                    l = m + 1
+                else:
+                    return m
+            if m < len(nums) and nums[m] == target:
+                return m
+            else:
+                return -1
+        return [findRange(True), findRange(False)]
